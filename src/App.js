ee2b593140c2e74f9932e58714f0bfe0f74c8f3f
Enahaque <enahaque1164@gmil.com>
2022-06-22 14:23:04 +0600

    updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Header from './components/Header/Header';
 
+
 function App() {
   return (
     <div className="App">
       <Header></Header>
       <h1 className="text-6xl text-orange-900 rounded-3xl ">Tailwind Pricing Club!!</h1>
+      
       
     </div>
   );
